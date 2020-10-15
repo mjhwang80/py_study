1f4f7d3007d1c8fa8296f636fdd6f9739c566787
mjhwang80 <hmjkor@gmail.com>
2020-10-16 00:35:02 +0900

    초기 프로젝트 추가

---
+++ module_sys.py
@@ -0,0 +1,17 @@
+import sys
+
+
+
+#컴퓨터 환경과 관련된 정보를 출력합니다.
+print (sys.argv)
+print ("---")
+# 컴퓨터 환경과 관련된 정보를 출력합니다.
+#print ("getwindowssversion:()", sys.getwindowsversion())
+print ("---")
+print ("copyright:", sys.copyright)
+print ("---")
+print ("version:", sys.version)
+
+#프로그램 강제 종료
+sys.exit()
+
